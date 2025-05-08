5b230572bce9ec9ca70a5ee8e95887e3cfdc4907
migue <migue@SNX>
2025-05-08 15:59:41 -0500

    Create Categories

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } // Será la llave primaria
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<Product> Products { get; set; } // Propiedad de navegación
+        public ICollection<Product>? Products { get; set; } = default!; // Propiedad de navegación
 
     }
 }
