49f98b5c3657ba4ad7eb8c76f6ad0654e558366e
migue <migue@SNX>
2025-05-08 14:37:52 -0500

    Index Categories

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private  readonly SumpermarketContext _context;
+        public IndexModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+        public IList<Category> Categories { get; set; } = default!; 
+        public async Task OnGetAsync()
+        {
+            if (_context.Categories != null)
+            {
+                Categories = await _context.Categories.ToListAsync();
+            }
+        }
+    }
+}
