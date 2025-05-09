2bc7d3f57f506181e8571d895646904fdf866ee0
migue <migue@SNX>
2025-05-09 13:47:33 -0500

    creation of delete product

---
+++ Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+            Product = await _context.Products
+            .Include(p => p.Category)
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                Product = product;
+                _context.Products.Remove(Product);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
