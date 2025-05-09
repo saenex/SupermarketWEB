b11148c64cd88d2a085ec25937dc2353795d2364
migue <migue@SNX>
2025-05-09 17:22:23 -0500

    Delete all Products and start over, beginning with creating the Products index

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -9,21 +9,17 @@ namespace SupermarketWEB.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly SupermarketContext _context;
-
         public IndexModel(SupermarketContext context)
         {
             _context = context;
         }
-
         public IList<Product> Products { get; set; } = default!;
-
         public async Task OnGetAsync()
         {
-          
-            Products = await _context.Products
-            .Include(p => p.Category)
-            .ToListAsync();
-            
+            if (_context.Products != null)
+            {
+                Products = await _context.Products.ToListAsync();
+            }
         }
     }
 }
